22d8ca550618935f1b0f5eeeca7d41cc22e12072
Joshua McDonald <mcdojj12@gmail.com>
2020-07-29 07:21:12 +1000

    added card styling to the strand info

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -5,14 +5,15 @@
 
     
     <!-- {{ safeBluesData.stats }} -->
-    <div v-for="item in safeBluesData.stats" :key="item.strandId" > 
-      <h3> Strain Id: {{item.strandId}} </h3>
-      <p>Times: {{item.times}}</p>
-      <p>Total incubating strands: {{item.totalIncubatingStrands}}</p>
-      <p>Total infected strands: {{item.totalInfectedStrands}}</p>
-      <p>Total removed strands: {{item.totalRemovedStrands}}</p>
-
-      </div>
+    <v-card class="mx-auto" max-width="344" v-for="item in safeBluesData.stats" :key="item.strandId" > 
+      <v-card-text>
+        <h3> Strain Id: {{item.strandId}} </h3>
+        <p>Times: {{item.times}}</p>
+        <p>Total incubating strands: {{item.totalIncubatingStrands}}</p>
+        <p>Total infected strands: {{item.totalInfectedStrands}}</p>
+        <p>Total removed strands: {{item.totalRemovedStrands}}</p>
+      </v-card-text>
+      </v-card>
     <!-- {{ this.arrayCovidApiData }} -->
   </div>
 </template>
