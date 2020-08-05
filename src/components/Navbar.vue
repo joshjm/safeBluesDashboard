18ac2c972dc2e73db1597de0dffe9d1a5fc88d4e
Joshua McDonald <mcdojj12@gmail.com>
2020-08-05 14:01:05 +1000

    added page-

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -8,7 +8,7 @@
     <v-spacer></v-spacer>
 
     <v-toolbar-items class="hidden-sm-and-down">
-      <v-btn @click="$router.push('/')" text> Home </v-btn>
+      <v-btn @click="$router.push('/')" text> Home Page  </v-btn>
       <v-btn @click="$router.push('/about')" text> About </v-btn>
       <v-btn @click="$router.push('/dashboard')" text> Dashboard </v-btn>
       <v-btn @click="$router.push('/admin')" text> Admin </v-btn>
